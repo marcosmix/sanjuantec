91ef88cef96a57146b8c7183ce60c4072aad84b6
LeanBrizu <leandrobri.ar@gmail.com>
2023-05-08 23:57:52 -0300

    Ordenar rutas en el archivo web.php.

--- routes/web.php
+++ routes/web.php
@@ -5,31 +5,31 @@ use App\Http\Controllers\CertificadoController;
 use App\Http\Controllers\CursosController;
 use App\Http\Controllers\DifusionController;
 use App\Http\Controllers\ContactoController;
-Route::get('/generarCertificados/{curso_id}',[CertificadoController::class,'index'])->name('generarCertificados');
-Route::post('/procesando',[CertificadoController::class,'generar'])->name('generarLectura');
+
+Route::get('/', function () {
+    return view('inicio');
+});
+
 Route::resource('cursos',CursosController::class);
 
-Route::view('/certificados/index','certificados.index-certificados')->name('curos-inicio');
+Route::get('/generarCertificados/{curso_id}',[CertificadoController::class,'index'])->name('generarCertificados');
+Route::post('/procesando',[CertificadoController::class,'generar'])->name('generarLectura');
 Route::post('/generarCertificadoEspeciales',[CertificadoController::class, 'generarCertificadoEspeciales'])->name('generarCertificadoEspeciales');
+Route::post('/generarCertificadoJam',[CertificadoController::class, 'generarCertificadoJam'])->name('generarCertificadoJam');
 
+Route::view('/certificados/index','certificados.index')->name('cursosInicio');
 Route::view('/ceftificados/especiales', 'certificados.certificadosEspecialesExcel')->name('certificadosEspecialesIndex');
-Route::get('/difusion', [DifusionController::class, 'index'])->name('difusion.index');
-Route::get('/difusionImportarAprobados', [DifusionController::class, 'CargarContactos'])->name('difusion.ImportarAprobados');
-Route::post('/emviarMailsAprobados', [DifusionController::class, 'EnviarCertificados'])->name('difusion.EnviarCertificados');
-
 Route::get('/cetificadoJAM',function(){
     return view('certificados.certificadoJAM');
 });
-Route::post('/generarCertificadoJam',[CertificadoController::class, 'generarCertificadoJam'])->name('generarCertificadoJam');
 
-Route::post('/importarContactos',[ContactoController::class, 'importarContactos'])->name('importarContactos');
-
+Route::get('/difusion', [DifusionController::class, 'index'])->name('difusion.index');
+Route::get('/difusionImportarAprobados', [DifusionController::class, 'CargarContactos'])->name('difusion.ImportarAprobados');
+Route::post('/emviarMailsAprobados', [DifusionController::class, 'EnviarCertificados'])->name('difusion.EnviarCertificados');
 Route::get('/enviarMails',[DifusionController::class,'enviarMails'])->name('enviarMail');
 Route::get('/EnviarMensaje',[DifusionController::class,'enviarMensaje'])->name('difusion.EnviarMensaje');
 Route::post('/EnviarMensaje',[DifusionController::class,'EnviarMensajePOST'])->name('difusion.EnviarMensajePOST');
 
-Route::get('/testearPDF',[CertificadoController::class, 'testearVistaPDF']);
+Route::post('/importarContactos',[ContactoController::class, 'importarContactos'])->name('importarContactos');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/testearPDF',[CertificadoController::class, 'testearVistaPDF']);
