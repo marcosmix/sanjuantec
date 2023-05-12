ef7ab26a647bc347b51fa1c82667811ea9c974e0
LeanBrizu <leandrobri.ar@gmail.com>
2023-05-11 23:31:02 -0300

    Modificar indentación de código PSR-2.

--- app/Http/Controllers/DifusionController.php
+++ app/Http/Controllers/DifusionController.php
@@ -11,63 +11,79 @@ use App\Imports\EstudiantesImport;
 use App\Imports\CursatecImport;
 use App\container\MensajesContainer;
 use App\Imports\CursatecImportFULL;
+
 class DifusionController extends Controller
 {
     use rutas;
 
-    public function index(){
-        return view('difusion.index');
+    public function index()
+    {
+        return view("difusion.index");
     }
 
-    
-    public function EnviarMensajePOST(Request $request){
-
-        $listado = (new CursatecImportFULL)->toArray($request->file('contactos'));
-        foreach($listado[0] as $estudiante){
+    public function EnviarMensajePOST(Request $request)
+    {
+        $listado = (new CursatecImportFULL())->toArray(
+            $request->file("contactos")
+        );
+        foreach ($listado[0] as $estudiante) {
             dump($estudiante);
-            MailTec::EnviarCuentasCursatec($this->rowToArrayCursatecCuentas($estudiante));
+            MailTec::EnviarCuentasCursatec(
+                $this->rowToArrayCursatecCuentas($estudiante)
+            );
         }
- 
-       return redirect()->back();
-    }
-    public function EnviarMensaje(){
-        return view('difusion.mensajes');
+
+        return redirect()->back();
     }
 
-    public function CargarContactos(){
-        $cursos=Curso::all();
-        return view('difusion.enviarCertificados',compact('cursos'));
+    public function EnviarMensaje()
+    {
+        return view("difusion.mensajes");
     }
 
-
-    
-    public function EnviarCertificados(Request $request){
-        $listado = (new EstudiantesImport)->toArray(request()->file('contactos'));
-        $curso = Curso::where('nombre',$request->curso)->first()->toArray();
-        $mensaje =MensajesContainer::difusionMarketing();
-        foreach($listado[0] as $estudiante)
-         MailTec::EnviarMailCertificados($this->rowToArray($estudiante),$curso,$mensaje);
- 
-       return redirect()->back();
-
+    public function CargarContactos()
+    {
+        $cursos = Curso::all();
+        return view("difusion.enviarCertificados", compact("cursos"));
     }
 
-    public function rowToArray($estudiante){
+    public function EnviarCertificados(Request $request)
+    {
+        $listado = (new EstudiantesImport())->toArray(
+            request()->file("contactos")
+        );
+        $curso = Curso::where("nombre", $request->curso)
+            ->first()
+            ->toArray();
+        $mensaje = MensajesContainer::difusionMarketing();
+        foreach ($listado[0] as $estudiante) {
+            MailTec::EnviarMailCertificados(
+                $this->rowToArray($estudiante),
+                $curso,
+                $mensaje
+            );
+        }
+
+        return redirect()->back();
+    }
+
+    public function rowToArray($estudiante)
+    {
         return [
-                'nombre'=>$estudiante[0],
-                'apellido'=>$estudiante[1],
-                'dni'=>$estudiante[2],
-                'email' => $estudiante[4],
+            "nombre" => $estudiante[0],
+            "apellido" => $estudiante[1],
+            "dni" => $estudiante[2],
+            "email" => $estudiante[4],
         ];
     }
 
-    public function rowToArrayCursatecCuentas($estudiante){
+    public function rowToArrayCursatecCuentas($estudiante)
+    {
         return [
-                'nombre'=>$estudiante[3],
-                'user'=>$estudiante[0],
-                'pass'=>$estudiante[1],
-                'email' => $estudiante[2],
+            "nombre" => $estudiante[3],
+            "user" => $estudiante[0],
+            "pass" => $estudiante[1],
+            "email" => $estudiante[2],
         ];
     }
-   
 }
