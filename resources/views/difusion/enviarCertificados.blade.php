b31c8ec2054a4d284b3b6feff2b2bd9b1482f351
LeanBrizu <leandrobri.ar@gmail.com>
2023-05-29 22:39:31 -0300

    Editar texto. Agregar comentario de trabajo pendiente. Agregar botón de Generar Certificados (falta la funcionalidad).

--- resources/views/difusion/enviarCertificados.blade.php
+++ resources/views/difusion/enviarCertificados.blade.php
@@ -8,10 +8,10 @@
             <h3 class="centrar-texto margen-superior-60px">{{ $datos['nombre'] }}</h3>
             <div class="div-borde-redondeado margen-superior-60px padding-total-10px">
                 <h3>Listado de alumnos que pertenecen a ese curso</h3>
-                <p>El listado de alumnos </p>
-                <p>Formato hoja de cálculo con tres columnas: Nombre, apellido, DNI, celular, mail.</p>
-                <p>Deben respetar el mismo orden y no se debe colocar encabezado. No pueden faltar los valores de las columnas DNI y email ya que se implementan para realizar el envio, los otros campos pueden estar en blanco.</p>
-                <p>Se recomienda usar el mismo archivo con el que generó los certificados</p>
+                <p><b>Formato del listado de alumnos:</b> </p>
+                <p>La hoja de cálculo debe contener hasta cuatro columnas: Nombre, apellido, DNI, celular, mail.</p>
+                <p>Deben respetar el mismo orden y no se debe colocar encabezado. No pueden faltar los valores de las columnas DNI y email ya que se implementan para realizar el envío. Los otros campos pueden estar en blanco.</p>
+                <p>Se recomienda usar el mismo archivo con el que generó los certificados.</p>
                 <input class="boton" type="file" name="contactos" id="listado" required>
             </div>
         </div>
@@ -21,8 +21,11 @@
         </div>
     </form>
 </div>
-
-<div class="centrar-texto">
+<!-- TODO Implementar la funcionalidad del botón Generar Certificados. -->
+<div class="centrar-texto margen-superior-20px">
+    <button type="submit" class="boton boton-alerta margen-superior-20px">Generar Certificados</button>
+</div>
+<div class="margen-superior-20px">
     <button id="boton-volver" type="submit" class="boton boton-aviso margen-superior-20px">Volver</button>
 </div>
 <script>
