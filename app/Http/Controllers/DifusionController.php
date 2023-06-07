da67cda96346566b227b25a25b46ea66ea539dc7
LeanBrizu <leandrobri.ar@gmail.com>
2023-06-06 23:49:41 -0300

    Importar Facade, container y Rule de Illuminate. Renombramiento de método. Conformación de método index(). Conformación preliminar de método EnviarCertificados().

--- app/Http/Controllers/DifusionController.php
+++ app/Http/Controllers/DifusionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Certificado;
 use App\Models\Curso;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
@@ -9,8 +10,10 @@ use App\Helpers\MailTec;
 use App\Helpers\rutas;
 use App\Imports\EstudiantesImport;
 use App\Imports\CursatecImport;
-use App\container\MensajesContainer;
 use App\Imports\CursatecImportFULL;
+use App\container\MensajesContainer;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class DifusionController extends Controller
 {
@@ -18,7 +21,8 @@ class DifusionController extends Controller
 
     public function index()
     {
-        return view("difusion.index");
+        $cursos = Curso::all();
+        return view("cursos.cardCurso", compact("cursos"));
     }
 
     public function EnviarMensajePOST(Request $request)
@@ -41,19 +45,14 @@ class DifusionController extends Controller
         return view("difusion.mensajes");
     }
 
-    public function generarCertificados(Request $request)
+    public function prepararEnvioCertificados (Request $request)
     {
         $titulo = "Certificados de cursos";
         $datos = $request->input('datos');
 
         $vista1 = view('base', compact('titulo'));
         $vista2 = view("difusion.importarContactos", compact('datos'));
-
         $vistaConcatenada = $vista1->render() . $vista2->render();
-        // TODO Aquí deberán ser implementadas las siguientes funcionalidades:
-        // - El llamado a otro método que generará los certificados.
-        // - El llamado a otro método que guardará los certificados (base de datos (+ ¿pdf?)).
-        // - El llamado a otro método que enviará el certificado vía email a cada alumno de un curso determinado.
 
         return $vistaConcatenada;
     }
@@ -72,22 +71,41 @@ class DifusionController extends Controller
 
     public function EnviarCertificados(Request $request)
     {
-        $listado = (new EstudiantesImport())->toArray(
-            request()->file("contactos")
-        );
+        // Validar archivo subido.
+        $validacion = Validator::make($request->all(), [
+            'contactos' => 'required|file|mimes:xlsx',
+        ]);
+
+        // Revisar si la validación falló.
+        if ($validacion->fails()) {
+            return redirect()->route('plantillas')->withErrors($validacion);
+        }
+
+        // Obtener, del Excel, una matriz de alumnos destinatarios.
+        $listado = (new EstudiantesImport())->toArray($request->file("contactos"));
+
+        // Obtener datos del curso, según su nombre.
         $curso = Curso::where("nombre", $request->curso)
             ->first()
             ->toArray();
+
+         // TODO Aquí, invocar método para la creación de los certificados.
+        $certificado = New Certificado();
+        $certificadoGenerado = $certificado->generarCertificadosPorCurso($curso, $listado);
+        die();
+        // Obtener mensaje.
         $mensaje = MensajesContainer::difusionMarketing();
-        foreach ($listado[0] as $estudiante) {
+
+        // Envío iterativo de emails con certificados.
+        foreach ($listado as $estudiante) {
             MailTec::EnviarMailCertificados(
-                $this->rowToArray($estudiante),
+                $estudiante,
                 $curso,
                 $mensaje
             );
         }
 
-        return redirect()->back();
+        return redirect()->route('plantillas');
     }
 
     public function rowToArray($estudiante)
