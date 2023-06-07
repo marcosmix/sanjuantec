552d78b7c5f0635e53ee520cc0905aae43a7fc2d
LeanBrizu <leandrobri.ar@gmail.com>
2023-06-07 00:01:40 -0300

    Agregar comprobación de existencia de variable para compatibilizar vista.

--- resources/views/difusion/importarContactos.blade.php
+++ resources/views/difusion/importarContactos.blade.php
@@ -9,21 +9,31 @@
                 <h3>Listado de alumnos que pertenecen a ese curso</h3>
                 <p><b>Formato del listado de alumnos:</b> </p>
                 <p>La hoja de cálculo debe contener hasta cuatro columnas: Nombre, apellido, DNI, celular, mail.</p>
-                <p>Deben respetar el mismo orden y no se debe colocar encabezado. No pueden faltar los valores de las columnas DNI y email ya que se implementan para realizar el envío.<br>
+                <p>Deben respetar el mismo orden y no se debe colocar encabezado. No pueden faltar los valores de las
+                 columnas DNI y email ya que se implementan para realizar el envío.<br>
                  Los otros campos pueden estar en blanco.</p>
                 <p>Se recomienda usar el mismo archivo con el que generó los certificados.</p>
+
+                @if (isset($datos['nombre']))
+                    <input type="hidden" name="curso" value="{{ $datos['nombre'] }}">
+                @endif
+
                 <input class="boton" type="file" name="contactos" id="listado" required>
             </div>
         </div>
         <br>
         <div class="div-borde-redondeado padding-total-10px">
-           <label><input type="checkbox" id="checkbox" name="enviar_email" value="true">Enviar vía e-mail todos los certificados al terminar de generarlos.</label><br>
+           <label>
+               <input type="checkbox" id="checkbox" name="enviar_email" value="true">
+                   Enviar vía e-mail todos los certificados al terminar de generarlos.
+           </label>
+           <br>
+        </div>
+        <!-- TODO Implementar la funcionalidad del botón Generar Certificados. -->
+        <div class="centrar-texto margen-superior-20px">
+            <button type="submit" class="boton boton-alerta margen-superior-20px">Generar Certificados</button>
         </div>
     </form>
-</div>
-<!-- TODO Implementar la funcionalidad del botón Generar Certificados. -->
-<div class="centrar-texto margen-superior-20px">
-    <button type="submit" class="boton boton-alerta margen-superior-20px">Generar Certificados</button>
 </div>
 
 <div class="margen-superior-20px">
