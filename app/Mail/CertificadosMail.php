bd55b13364b6b7342d341eaf0b737b15c8a35dd7
LeanBrizu <leandrobri.ar@gmail.com>
2023-06-06 23:57:03 -0300

    Refactorizar mailer CertificadosMail. Puesta en marcha. Falta probar la funcionalidad de archivos pdf (certificado) adjunto.

--- app/Mail/CertificadosMail.php
+++ app/Mail/CertificadosMail.php
@@ -12,26 +12,37 @@ class CertificadosMail extends Mailable
     use Queueable, SerializesModels,rutas;
 
     public $estudiante,$curso,$mensaje;
-  
-    public function __construct($estudiante,$curso, $mensaje='Test '){
-        $this->estudiante= $estudiante;
+
+    public function __construct($estudiante, $curso, $mensaje='Test ')
+    {
+        $this->estudiante = $estudiante;
         $this->curso = $curso;
-        $this->mensaje=$mensaje;
+        $this->mensaje = $mensaje;
     }
 
-    
 
-    public function build(){
-        
-        
-        return $this->from('sanjuantec@sjtec.com','San Juan Tec')->view('mail.certificado')
-                ->subject('San Juan Tec')
-                ->replyTo('sanjuantec@sjtec.com','SAN JUAN TEC')
-                ->attach(public_path($this->RutaCarpetaStoragePublic($this->curso,$this->estudiante)),[
-                    'as'=> $this->NombreArchivoPDF($this->estudiante),
-                    'mime'=> 'application/pdf'
-                ]);
 
-                
+    public function build()
+    {
+        $curso = $this->curso['nombre'];
+        $dniEstudiante = $this->estudiante->dni;
+        $rutaArchivoAdjunto = public_path('storage/certificados/'. $curso .'/' . $dniEstudiante . '.pdf');
+
+        // Verificación de archivo de certificado .pdf
+        if (!file_exists($rutaArchivoAdjunto)) {
+            // Add debugging statement to check the existence of the file
+            dd("El archivo adjuntable no ha sido encontrado en el directorio: " . $rutaArchivoAdjunto);
+        }
+
+        // Envío de email
+        return $this->from('sanjuantec@sjtec.com', 'San Juan Tec')
+            ->view('mail.certificado')
+            ->subject('San Juan Tec')
+            ->replyTo('sanjuantec@sjtec.com', 'SAN JUAN TEC')
+            ->attach($rutaArchivoAdjunto, [
+                'as' => $this->NombreArchivoPDF($this->estudiante),
+                'mime' => 'application/pdf'
+            ]);
     }
+
 }
