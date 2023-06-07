591a1178ab7ef9898406f408ba43e7d566221a08
LeanBrizu <leandrobri.ar@gmail.com>
2023-06-07 00:02:42 -0300

    Mejorar indentación de código.

--- resources/views/mail/certificado.blade.php
+++ resources/views/mail/certificado.blade.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-</head>
-<body>
-        <h1>Estimada/o: {{$estudiante['nombre']}} 
-       </h1>
-        {{-- {{$curso['nombre']}} --}}
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    </head>
+    <body>
+        <h1>Estimada/o: {{ $estudiante['nombre'] }}
+        </h1>
+        {{-- {{ $curso['nombre'] }} --}}
         <br>
-              
         <p>{!! $mensaje !!}</p>
-    
-</body>
+    </body>
 </html>
