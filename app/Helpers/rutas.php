cf6bd6c2607a364e1c800591437dfec9717fe95a
LeanBrizu <leandrobri.ar@gmail.com>
2023-06-08 23:58:05 -0300

    Uso de constante de separador de directorios para compatibilizar con diversos sistemas operativos.

--- app/Helpers/rutas.php
+++ app/Helpers/rutas.php
@@ -2,24 +2,24 @@
 namespace App\Helpers;
 
 trait rutas{
-    protected $raiz='certificados\\';
+    protected $raiz='certificados'.DIRECTORY_SEPARATOR;
 
     public function GenerarRutaPDF($curso, $estudiante){
-        return $this->raiz. $curso['nombre'] . "\\" . strval($estudiante['dni']) . ".pdf";
+        return $this->raiz. $curso['nombre'].DIRECTORY_SEPARATOR. strval($estudiante['dni']) . ".pdf";
     }
 
     public function GenerarRutaPDFv2($carpeta, $pdf){
-        return $this->raiz . $carpeta . "\\" .$pdf. ".pdf";
+        return $this->raiz.$carpeta .DIRECTORY_SEPARATOR.$pdf. ".pdf";
     }
 
 
     public function RutaCarpetaStoragePublic($curso, $estudiante){
-        return 'storage'."\\".$this->raiz . $curso['nombre'].'\\' . strval($estudiante['dni']) . ".pdf";
+        return 'storage'.DIRECTORY_SEPARATOR.$this->raiz . $curso['nombre'].DIRECTORY_SEPARATOR. strval($estudiante['dni']).".pdf";
     }
 
     public function RutaCarpetaStorage($curso)
     {
-        return $this->raiz . $curso['nombre'] . '\\';
+        return 'storage'.DIRECTORY_SEPARATOR.$this->raiz . $curso['nombre'].DIRECTORY_SEPARATOR;
     }
 
     public function NombreArchivoPDF($estudiante){
