d2c335a283dd154d1d3762f6de9d4aec70b5342c
LeanBrizu <leandrobri.ar@gmail.com>
2023-06-17 20:37:05 -0300

    Modificar el estilo de uso de llaves para que sea consistente con el resto de la aplicación.

--- app/Helpers/rutas.php
+++ app/Helpers/rutas.php
@@ -1,28 +1,37 @@
 <?php
 namespace App\Helpers;
 
-trait rutas{
+trait rutas
+{
     protected $raiz='certificados'.DIRECTORY_SEPARATOR;
 
-    public function GenerarRutaPDF($curso, $estudiante){
+    public function GenerarRutaPDF ($curso, $estudiante)
+    {
         return $this->raiz. $curso['nombre'].DIRECTORY_SEPARATOR. strval($estudiante['dni']) . ".pdf";
     }
 
-    public function GenerarRutaPDFv2($carpeta, $pdf){
+    public function GenerarRutaPDFv2 ($carpeta, $pdf)
+    {
         return $this->raiz.$carpeta .DIRECTORY_SEPARATOR.$pdf. ".pdf";
     }
 
+    public function RutaCarpeta ($curso)
+    {
+        return $this->raiz.$curso['nombre'].DIRECTORY_SEPARATOR;
+    }
 
-    public function RutaCarpetaStoragePublic($curso, $estudiante){
+    public function RutaCarpetaStoragePublic ($curso, $estudiante)
+    {
         return 'storage'.DIRECTORY_SEPARATOR.$this->raiz . $curso['nombre'].DIRECTORY_SEPARATOR. strval($estudiante['dni']).".pdf";
     }
 
-    public function RutaCarpetaStorage($curso)
+    public function RutaCarpetaStorage ($curso)
     {
         return 'storage'.DIRECTORY_SEPARATOR.$this->raiz . $curso['nombre'].DIRECTORY_SEPARATOR;
     }
 
-    public function NombreArchivoPDF($estudiante){
+    public function NombreArchivoPDF ($estudiante)
+    {
         return strval($estudiante['dni']).".pdf";
     }
 }
