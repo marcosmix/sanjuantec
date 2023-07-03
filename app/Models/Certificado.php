114844e73f5484f177b8da60c80ba152ec2959c7
LeanBrizu <leandrobri.ar@gmail.com>
2023-07-03 20:08:32 -0300

    Importar excepción ModelNotFoundException.

--- app/Models/Certificado.php
+++ app/Models/Certificado.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Helpers\gpdf;
 use App\Helpers\rutas;
 
