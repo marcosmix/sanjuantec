49cbf3424f932a1bdb5c92b4549743124127249d
LeanBrizu <leandrobri.ar@gmail.com>
2023-07-19 19:42:46 -0300

    [Mejora] Agregar método que define la relación entre el modelo Curso y el modelo Certificado.

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -11,6 +11,18 @@ class Curso extends Model
     protected $guarded=[];
     use HasFactory;
 
+    public function certificados()
+    {
+        return $this->hasMany(Certificado::class, 'id_curso', 'id');
+    /**
+     * Este método establece la relación "hasMany" entre el modelo actual y el modelo Certificado.
+     * Indica que un objeto de este modelo tiene múltiples instancias de Certificado relacionadas
+     * a través del campo 'id_curso'.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany La relación "hasMany" entre el modelo
+     * actual y el modelo Certificado.
+     */
+    }
+
     public function programa()
     {
         return ProgramasContainer::ver($this->programa_id)['nombre'];
