8b39b74b4168f13dab8a67ec20999931535adc7a
LeanBrizu <leandrobri.ar@gmail.com>
2023-07-27 21:08:14 -0300

    [Modificación] Implementar validación en guardarPlantilla.

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Curso;
-use Illuminate\Http\Request;
-use App\container\ProgramasContainer;
 use App\Helpers\CursaTec;
+use App\Models\Curso;
+use App\container\ProgramasContainer;
+use Illuminate\Http\Request;
 
 class CursosController extends Controller
 {
@@ -31,15 +31,12 @@ class CursosController extends Controller
 
     public function guardarPlantilla (Request $request)
     {
-        // TODO Implementar validación de datos.
-        $nuevoCurso = new Curso();
-        $nuevoCurso->create([
-            "nombre" => $request->nombre,
-            "texto" => $request->texto,
-            "duracion" => $request->duracion,
-            "fecha" => $request->fecha,
-            "bloque" => $request->bloque,
-        ]);
+        $resultado = Curso::validarYCrearCurso($request);
+
+        if (isset($resultado['errores_de_validacion'])) {
+            $programas = ProgramasContainer::listar();
+            return view("cursos.crearPlantilla", compact('programas'))->withErrors($resultado['errores_de_validacion']);
+        }
 
         return redirect(route("plantillas"));
     }
@@ -60,8 +57,8 @@ class CursosController extends Controller
         endforeach;
     /**
      * Este método obtiene todos los cursos que están presentes en Moodle/CursaTec.
-     * TODO Ha sido creado para poner
-     * a prueba la integración con la funcionalidad de Servicios Externos de Moodle.
+     * TODO Ha sido creado para poner a prueba la integración con la funcionalidad
+     * de Servicios Externos de Moodle.
      * @author Leandro Brizuela
      * @date 23 de mayo de 2023.
      */
