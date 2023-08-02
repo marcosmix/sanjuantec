21fe9f1c658d64c6fdf4f1c90af97106c97a3a01
LeanBrizu <leandrobri.ar@gmail.com>
2023-08-01 23:19:19 -0300

    [Modificación] Modificar nombre de elementos <input> para que sean consistentes al resto de la aplicación.

--- resources/views/difusion/importarListado.blade.php
+++ resources/views/difusion/importarListado.blade.php
@@ -8,10 +8,10 @@
     <br>
     <div class="borde-rojo div-borde-redondeado padding-total-10px">
         <div class="padding-total-10px">
-           <label><input type="checkbox" id="checkbox1" name="firmas_autoridades" value="true">Firmas de autoridades.</label><br>
+           <label><input type="checkbox" id="checkbox1" name="tieneFirmas" value="true">Firmas de autoridades.</label><br>
         </div>
         <div class="padding-total-10px">
-           <label><input type="checkbox" id="checkbox2" name="enviar_email" value="true">Enviar e-mail después de guardar.</label><br>
+           <label><input type="checkbox" id="checkbox2" name="enviarEmail" value="true">Enviar e-mail después de guardar.</label><br>
         </div>
     </div>
 </div>
