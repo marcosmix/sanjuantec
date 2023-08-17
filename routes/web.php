41324d762ce8128c18aebcc56c6cc968d730c2b8
LeanBrizu <leandrobri.ar@gmail.com>
2023-08-16 23:20:01 -0300

    [Modificación] Ampliación de Ruta para Visualización de Certificado PDF y Ordenamiento Alfabético de Archivos Importados
    Se ha agregado una nueva ruta destinada a la presentación de certificados en formato PDF. Además, se ha implementado un ordenamiento alfabético para los archivos importados. Para llevar a cabo esta funcionalidad, se ha introducido un nuevo controlador denominado PdfController. Estos cambios mejoran la estructura y accesibilidad de la aplicación al permitir una visualización más eficiente y una organización más clara de los archivos importados.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CertificadoController;
+use App\Http\Controllers\ContactoController;
 use App\Http\Controllers\CursosController;
 use App\Http\Controllers\DifusionController;
-use App\Http\Controllers\ContactoController;
+use App\Http\Controllers\PdfController;
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,7 +26,7 @@ Route::middleware(['auth'])->group(function () {
     return view('certificados.index');
     });
 
-    // Menú Crear plantillas de cursos ↓
+    // Menú Crear plantillas de cursos. ↓
     Route::get('/plantillas', [CursosController::class, 'index'])->name('plantillas');
     Route::get('/plantillas/crear', [CursosController::class, 'crearPlantilla'])->name('crearPlantilla');
     Route::post('/plantillas/crear', [CursosController::class, 'guardarPlantilla'])->name('guardarPlantilla');
@@ -43,8 +44,11 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/certificado/alumno', [DifusionController::class, 'generarCertificadoPorAlumno'])->name('generarCertificadoPorAlumno');
     Route::post('/certificado/crear', [DifusionController::class, 'enviarCertificadoPorAlumno'])->name('enviarCertificadoPorAlumno');
 
-    // Menú Administrar certificados de cursos ↓
+    // Menú Administrar certificados de cursos. ↓
     Route::get('/certificados/administrar', [CertificadoController::class, 'administrarCertificados'])->name('administrarCertificados');
+
+    // Ruta al controlador del método para visualizar un archivo PDF. ↓
+    Route::get('/mostrarPdf/{documento}/{nombreCurso}', [PdfController::class, 'mostrarPdf'])->name('mostrarPdf');
 
     /**
      * A partir de aquí, están definidas las rutas que deberán ser revisadas para ser refactorizadas, descartadas o simplemente utilizadas.
@@ -52,26 +56,26 @@ Route::middleware(['auth'])->group(function () {
      * @date 02 de junio de 2023
      */
 
-    // Menú 'Generar Certificados' ↓
+    // Menú 'Generar Certificados'. ↓
     Route::get('/generarCertificados/{curso_id}',[CertificadoController::class,'index'])->name('generarCertificados');
     Route::post('/procesando',[CertificadoController::class,'generar'])->name('generarLectura');
     Route::post('/generarCertificadoEspeciales',[CertificadoController::class, 'generarCertificadoEspeciales'])->name('generarCertificadoEspeciales');
     Route::post('/generarCertificadoJam',[CertificadoController::class, 'generarCertificadoJam'])->name('generarCertificadoJam');
 
-    // Menú 'Certificados' ↓
+    // Menú 'Certificados'. ↓
     Route::view('/certificados/index','certificados.index')->name('cursosInicio');
     Route::view('/certificados/especiales', 'certificados.certificadosEspecialesExcel')->name('certificadosEspecialesIndex');
     Route::get('/cetificadoJAM',function(){
         return view('certificados.certificadoJAM');
     });
 
-    // Menú 'Difusión' ↓
+    // Menú 'Difusión'. ↓
     Route::get('/difusion', [DifusionController::class, 'index'])->name('difusion.index');
     Route::get('/enviarMails',[DifusionController::class,'enviarMails'])->name('enviarMail');
     Route::get('/EnviarMensaje',[DifusionController::class,'enviarMensaje'])->name('difusion.EnviarMensaje');
     Route::post('/EnviarMensaje',[DifusionController::class,'EnviarMensajePOST'])->name('difusion.EnviarMensajePOST');
 
-    // Rutas a funcionalidades independientes ↓
+    // Rutas a funcionalidades independientes. ↓
     Route::post('/importarContactos',[ContactoController::class, 'importarContactos'])->name('importarContactos');
     Route::get('/testearPDF',[CertificadoController::class, 'testearVistaPDF']);
 
