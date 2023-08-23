ee7902902194f5798c64905f03bfaf8eced708b7
LeanBrizu <leandrobri.ar@gmail.com>
2023-08-22 21:57:13 -0300

    Agregado de CDN de Ajax y token CSRF en la sección <head> para funcionalidad Ajax
    En esta actualización, se ha implementado la inclusión del CDN de Ajax y se ha añadido el token CSRF en la sección <head> del archivo para habilitar la funcionalidad Ajax. Estos cambios permiten establecer una comunicación segura y efectiva con el servidor a través de solicitudes asíncronas, mejorando la interactividad y la experiencia del usuario en la aplicación.

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,9 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>San Juan Tec</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="/js/Boton.js"></script>
     </head>
     <body>
